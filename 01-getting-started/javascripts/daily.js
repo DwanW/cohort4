a0b9c8c74c417eda5b773a2ef07c58d939f7670a
DwanW <kunvang@yahoo.com>
2020-03-04 14:02:51 -0700

    adding comment

--- 01-getting-started/javascripts/daily.js
+++ 01-getting-started/javascripts/daily.js
@@ -1,3 +1,13 @@
+// TDD : Test Driven Development
+// it 1.reduces bugs to legacy projects, 2. decrease bug to code ratio to new projects, 
+// 3. increase software and code quality
+// automated testing is done through automation tool.
+// unit testing is the testing of individual units or components, fixes bug early in development cycle
+// and save cost.
+// test automation should be left developers because they know how to fix if there's an error.
+
+
+
 const functions = {
     convertToFahrenheit: (temp) => {
        return (temp * 9 /5) + 32;
