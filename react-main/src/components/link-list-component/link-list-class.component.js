8500cee5a724a2d0d773bdfde015808bb5214ffc
DwanW <kunvang@yahoo.com>
2020-03-23 16:02:13 -0600

    initialization

---
+++ react-main/src/components/link-list-component/link-list-class.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// class ListNode extends React.Component(){
+//     show(){
+//         return `subject: ${this.subject}, amount: ${this.amount}`;
+//     }
+
+// }
+
+class LinkedList extends React.Component{
+    constructor(){
+        this.head = null
+        this.tail = this.head;
+    }
+}
