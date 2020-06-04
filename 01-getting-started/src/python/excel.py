db2d16b88e6f0b666033daa04de91f3010ca70af
DwanW <kunvang@yahoo.com>
2020-06-04 11:03:54 -0600

    created invoice from the worksheet data

--- 01-getting-started/src/python/excel.py
+++ 01-getting-started/src/python/excel.py
@@ -1,4 +1,6 @@
 from openpyxl import load_workbook
+import os
+import datetime
 
 wb = load_workbook('./data_tables/shopdata.xlsx')
 
@@ -37,7 +39,37 @@ def storeInvoiceList(ws, targetDict):
             targetDict[invoice_key] = [{header_list[0].value: row[0].value, header_list[2].value: row[2].value}]
 
 
-# storeWbIntoDict(customer_ws,customer_dict)
-# storeWbIntoDict(product_ws,product_dict)
-# storeWbIntoDict(invoice_ws,invoice_dict)
-# storeInvoiceList(invoice_list_ws, invoice_list_dict)
+storeWbIntoDict(customer_ws,customer_dict)
+storeWbIntoDict(product_ws,product_dict)
+storeWbIntoDict(invoice_ws,invoice_dict)
+storeInvoiceList(invoice_list_ws, invoice_list_dict)
+
+def createInvoice(invoiceID):
+    if invoiceID not in invoice_dict:
+        return "invoice does not exist"
+    
+    customerID = invoice_dict[invoiceID]["Customer_ID"]
+    date = invoice_dict[invoiceID]['Date_issued']
+    firstName = customer_dict[customerID]['FirstName']
+    lastName = customer_dict[customerID]['LastName']
+    email = customer_dict[customerID]['Email']
+    invoice_item = invoice_list_dict[invoiceID]
+
+    invoice_item_chart = []
+    total = 0
+    for item in invoice_item: 
+        itemName = product_dict[item['Item_ID']]['Item_name']
+        itemUnitPrice = product_dict[item['Item_ID']]['Item_price']
+        quantity = item['Quantity']
+        total = total + itemUnitPrice * quantity
+        invoice_item_chart.append({"Item":itemName, "Quantity": quantity, "Price": itemUnitPrice, "Amount": quantity * itemUnitPrice})
+    
+    with open('invoice.txt', mode='w') as invoice:
+        invoice.write(f'Invoice For:\n {firstName} {lastName} \n {email} \n \n')
+        invoice.write(f'Invoice ID: {invoiceID} \n Sent: {date.date()} \n Due: {(date + datetime.timedelta(days=14)).date()} \n \n' )
+        invoice.write(f"ITEM            Quantity    Unit Price      Amount \n \n")
+        for item in invoice_item_chart:
+            invoice.write(f'{item["Item"]}          {item["Quantity"]}        ${item["Price"]}.00            ${item["Amount"]}.00 \n \n')
+        invoice.write(f"SUBTOTAL:${total}.00\n TAX:${round(total * 0.1, 2)}")
+        
+createInvoice(1)
